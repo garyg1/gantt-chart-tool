7085e0e107aab6fc41558970d8f02b23957f6439
Gary Gurlaskie <garygurlaskie@gmail.com>
2024-08-18 10:56:15 -0700

    improve @file description

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * @file The entire timline renderer/optimizer application is contained in this file.
  * The design goal is simplicity and minimizing dependencies. It is pure JavaScript,
- * does not require a bundler or framework, and will run on any HTML page that contains
- * the below global HTMLElements.
+ * does not require a bundler or framework, and can be loaded by any HTML page
+ * that contains the below global HTMLElements.
  *
  * @license
  * Copyright (c) 2023-2024 Gary Gurlaskie
